ebe283ff143f81e52f3dc298e9e3f4dddc3d5402
Shaurya Pandey <shauryapandey40@gmail.com>
2026-09-04 00:37:02 +0530

    feat(client): register /meetings/:id route in App router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
+import { MeetingDetails } from './pages/meetingsDetails';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/meetings/:id" element={<MeetingDetails />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
